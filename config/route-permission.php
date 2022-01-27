14b4630b4e8b447c2926b92448b2ab5a6acfd617
radiatecode <radiate126@gmail.com>
2022-01-27 20:48:03 +0600

    permissioble route modified, config updated, cache implimented

--- config/route-permission.php
+++ config/route-permission.php
@@ -3,6 +3,13 @@
 
 return [
     /**
+     * Allowable controller namespace will be count as permissible routes
+     */
+    'allowable-controller-namespace' => [
+        'App\Http\Controllers',
+    ],
+
+    /**
      * Split route by defined character
      */
     'route-name-splitter' => '.',
@@ -15,13 +22,21 @@ return [
     ],
 
     /**
+     * Cache the permissible routes
+     */
+    'cache-routes' => [
+      'cacheable' => true,
+      'cache-driver' => env('CACHE_DRIVER', 'file')
+    ],
+
+    /**
      * permission button used to checked / unchecked all routes
      *
      * nt: extra button can be added
      */
     'permission-buttons' => [
         '<button type="button" class="btn btn-success" onclick="checkAll()"><i class="fa fa-check-square"></i> Check All</button>',
-        '<button type="button" class="btn btn-warning" onclick="uncheckAll()"><i class="fa fa-square"></i> Uncheck All </button>'
+        '<button type="button" class="btn btn-warning" onclick="uncheckAll()"><i class="fa fa-square"></i> Uncheck All </button>',
     ],
 
     /**
--- src/PermissibleRoutes.php
+++ src/PermissibleRoutes.php
@@ -4,79 +4,148 @@
 namespace RadiateCode\LaravelRoutePermission;
 
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
 use RadiateCode\LaravelRoutePermission\Contracts\WithPermissible;
 
 class PermissibleRoutes
 {
-    private const ALLOWABLE_NAMESPACE = 'App\Http\Controllers';
+    private const CACHE_ROUTES_COUNT_KEY = 'routes-count';
+
+    private const CACHE_ROUTES_KEY = 'routes';
 
     public static function getRoutes(): array
     {
         $routes = Route::getRoutes();
+
+        $routesCount = count($routes);
+
+        $cacheRoutes = self::getCacheRoutes($routesCount);
+
+        if ($cacheRoutes !== null) {
+            return $cacheRoutes;
+        }
+
+        $globalExcludeRoutes = config('route-permission.exclude-routes');
 
         $permissibleRoutes = [];
 
         $current = [];
 
         foreach ($routes as $route) {
-            $action = $route->getAction();
-
-            $namespace = $action['namespace'];
-
-            if (Str::contains($namespace, self::ALLOWABLE_NAMESPACE)) {
-                $actionName = $route->getActionName();
-
-                $actionExtract = explode('@', $actionName);
-
-                $controllerExtract = $actionExtract[0];
-
-                if ($controllerExtract == 'Closure') {
-                    continue;
-                }
-
-                $controllerMethod = $actionExtract[1];
-
-                $controller = '\\'.$controllerExtract;
-
-                $controllerInstance = app($controller);
-
-                if ($controllerInstance instanceof WithPermissible) {
-                    $title = $controllerInstance->getPermissionTitle();
-
-                    // if no title defined then generate title from controller name
-                    if (empty($title)) {
-                        $controllerName = class_basename($controllerExtract);
-
-                        if (Str::contains($controllerName, 'Controller')) {
-                            $title = str_replace('Controller', ' Permission',
-                                $controllerName);
-                        }
-                    }
-
-                    $excludeRoutes = $controllerInstance->getExcludeRoutes();
-
-                    $excludeMethods = $controllerInstance->getExcludeMethods();
-
-                    $key = strtolower(Str::slug($title, "-"));
-
-                    if (in_array($route->getName(), $excludeRoutes)
-                        || in_array($controllerMethod, $excludeMethods)
-                        || in_array($route->getName(), $current)
-                    ) {
-                        continue;
-                    }
-
-                    $permissibleRoutes[$key][] = $route->getName();
-
-                    $current
-                        = $permissibleRoutes[$key]; // avoid duplicate route entry
-                }
+            if (in_array($route->getName(), $globalExcludeRoutes)) {
+                continue;
             }
+
+            $actionName = $route->getActionName();
+
+            $actionExtract = explode('@', $actionName);
+
+            $controllerExtract = $actionExtract[0];
+
+            if ($controllerExtract == 'Closure') {
+                continue;
+            }
+
+            if ( ! self::isNamespaceAllowable($controllerExtract)) {
+                continue;
+            }
+
+            $controller = '\\'.$controllerExtract;
+
+            $controllerInstance = app($controller);
+
+            if ( ! $controllerInstance instanceof WithPermissible) {
+                continue;
+            }
+
+            $controllerMethod = $actionExtract[1];
+
+            $excludeRoutes = $controllerInstance->getExcludeRoutes();
+
+            $excludeMethods = $controllerInstance->getExcludeMethods();
+
+            if (in_array($route->getName(), $excludeRoutes)
+                || in_array($controllerMethod, $excludeMethods)
+                || in_array($route->getName(), $current)
+            ) {
+                continue;
+            }
+
+            $title = self::permissionTitle($controllerInstance);
+
+            $key = strtolower(Str::slug($title, "-"));
+
+            $permissibleRoutes[$key][] = $route->getName();
+
+            $current = $permissibleRoutes[$key]; // avoid duplicate route entry
         }
+
+        self::cacheRoutes($routesCount, $permissibleRoutes);
 
         return $permissibleRoutes;
     }
 
+    protected static function permissionTitle($controllerInstance)
+    {
+        $title = $controllerInstance->getPermissionTitle();
+
+        if ( ! empty($title)) {
+            return $title;
+        }
+
+        // if no title defined then generate title from controller name
+        $controllerName = class_basename($controllerInstance);
+
+        // place white space between controller (PascalCase) name
+        $name = preg_replace('/([a-z])([A-Z])/s', '$1 $2', $controllerName);
+
+        if (Str::contains($controllerName, 'Controller')) {
+            return str_replace('Controller', 'Permission', $name);
+        }
+
+        return $name.' Permission';
+    }
+
+    protected static function isNamespaceAllowable($namespace): bool
+    {
+        $allowableNamespaces = config('route-permission.allowable-controller-namespace');
+
+        foreach ($allowableNamespaces as $allowableNamespace) {
+            if (Str::contains($namespace, $allowableNamespace)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    protected static function getCacheRoutes(int $routesCount)
+    {
+        if ( ! config('route-permission.cache-routes.cacheable')
+            || ! Cache::has(self::CACHE_ROUTES_COUNT_KEY)
+        ) {
+            return null;
+        }
+
+        if ($routesCount !== Cache::get(self::CACHE_ROUTES_COUNT_KEY)) {
+            Cache::forget(self::CACHE_ROUTES_COUNT_KEY);
+            Cache::forget(self::CACHE_ROUTES_KEY);
+
+            return null;
+        }
+
+        return Cache::get(self::CACHE_ROUTES_KEY);
+    }
+
+    protected static function cacheRoutes(int $routesCount, $permissibleRoutes)
+    {
+        if (config('route-permission.cache-routes.cacheable')
+            && ! Cache::has(self::CACHE_ROUTES_COUNT_KEY)
+        ) {
+            Cache::put(self::CACHE_ROUTES_COUNT_KEY, $routesCount,now()->addDay());
+            Cache::put(self::CACHE_ROUTES_KEY, $permissibleRoutes,now()->addDay());
+        }
+    }
 }
